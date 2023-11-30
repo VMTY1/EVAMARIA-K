adb39ece97e8da14c41931c36d6c036520bbfabb
MAYAVI <105354161+Mayavi333@users.noreply.github.com>
2023-11-30 22:56:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,7 +23,7 @@ async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [
             [
-                InlineKeyboardButton('📣ᴜᴩᴅᴀᴛᴇꜱ📣', url='https://t.me/KOMBOTZZ')
+                InlineKeyboardButton('📣ᴜᴩᴅᴀᴛᴇꜱ📣', callback_data='https://t.me/KOMBOTZZ')
             ],
             [
                 InlineKeyboardButton('⚙️ʜᴇʟᴩ⚙️', url=f"https://t.me/{temp.U_NAME}?start=help"),
